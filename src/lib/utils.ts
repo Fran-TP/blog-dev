7267e4eb6596abbac614833816e505b6dcb273e1
Fran-TP <ftorresp@unasam.edu.pe>
2025-03-19 23:05:38 -0500

    feat: add pagination utility function for improved navigation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,32 @@ export const formatDate = (date: string): string => {
 export const convertToSlug = (slug: string) => {
   return slug.replace(/\s+/g, '-').toLowerCase()
 }
+
+export const generatePagination = (currentPage: number, totalPages: number) => {
+  const MIN_PAGES = 7
+  const FIRST_PAGE_LIMIT = 3
+
+  if (totalPages <= MIN_PAGES) {
+    return Array.from({ length: totalPages }, (_, i) => i + 1)
+  }
+
+  if (currentPage <= FIRST_PAGE_LIMIT) {
+    return [1, 2, 3, '...', totalPages - 1, totalPages]
+  }
+
+  // if the current page is in the 3 last pages
+  if (currentPage >= totalPages - 2) {
+    return [1, 2, '...', totalPages - 2, totalPages - 1, totalPages]
+  }
+
+  // if the current page is in then middle
+  return [
+    1,
+    '...',
+    currentPage - 1,
+    currentPage,
+    currentPage + 1,
+    '...',
+    totalPages,
+  ]
+}
