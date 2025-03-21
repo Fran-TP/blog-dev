0b5abd08b7e1feeea6876d9ff268eb784405a83a
Fran-TP <ftorresp@unasam.edu.pe>
2025-03-20 22:38:16 -0500

    feat: add width and height properties to image object for enhanced layout control

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,8 @@ const posts = defineCollection({
     image: z.object({
       url: z.string().url(),
       alt: z.string(),
+      width: z.number(),
+      height: z.number()
     }),
     tags: z.array(z.string()),
   }),
